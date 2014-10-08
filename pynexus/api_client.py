26a6da62dc81720ea13645589719dcbae6dadacc
Ricardo Carrillo Cruz <ricardo.carrillo.cruz@gmail.com>
2014-10-08 10:53:37 +0200

    Refactor url attribute to uri
    It's better to construct the uri in the class constructor, instead
    of constructing it in every single REST method

--- pynexus/api_client.py
+++ pynexus/api_client.py
@@ -2,16 +2,16 @@ import requests
 
 class ApiClient:
     def __init__(self, host, username, password):
-        self.host = host
+        self.uri = host + '/nexus/service/local/'
         self.username = username
         self.password = password
 
     def get_all_repositories(self):
-        r = requests.get(self.host+'/nexus/service/local/repositories', headers={'Accept': 'application/json'})
+        r = requests.get(self.uri + 'all_repositories', headers={'Accept': 'application/json'})
 
         return r
 
     def get_status(self):
-        r = requests.get(self.host+'/nexus/service/local/status', headers={'Accept': 'application/json'})
+        r = requests.get(self.uri + 'status', headers={'Accept': 'application/json'})
         
         return r
