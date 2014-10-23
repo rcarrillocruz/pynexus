e064d00c30e4ca2e32cb8f528c13de316032ce77
Ricardo Carrillo Cruz <ricardo.carrillo.cruz@gmail.com>
2014-10-23 20:56:50 +0200

    Add test_get_users_return_list_with_just_anonymous_user test
    test_get_users_return_list_with_just_anonymous_user is a test
    that mocks requests.get call to make it return a JSON containing
    a list users with just anonymous user details.
    This is to test the get_users call.

--- tests/test_nexus.py
+++ tests/test_nexus.py
@@ -1,4 +1,5 @@
 import unittest
+from mock import patch
 from pynexus import api_client
 
 class NexusTest(unittest.TestCase):
@@ -7,3 +8,16 @@ class NexusTest(unittest.TestCase):
         n = api_client.ApiClient('http://test.com', 'testuser', 'testpwd')
         self.assertEquals(n.uri, 'http://test.com/nexus/service/local/')
         
+    @patch.object(api_client.requests, 'get')    
+    def test_get_users_return_list_with_just_anonymous_user(self, mock_get):
+        mock_output = u'{"data":[{"resourceURI":"http://test.com/nexus/' \
+                       'service/local/users/anonymous","userId":"anonymous",' \
+                       '"firstName":"Nexus","lastName":"Anonymous User",' \
+                       '"status":"active","email":"changeme2@yourcompany.com"' \
+                       ',"roles":["anonymous","repository-any-read"]}'
+
+        mock_get.return_value = mock_output
+        n = api_client.ApiClient('http://test.com', 'testuser', 'testpwd')
+        result = n.get_users()
+        self.assertEqual(result, mock_output)
+
